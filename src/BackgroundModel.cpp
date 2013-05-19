7c9c4f4259fb584e9b45710e4b2d98ec10bf78e7
Piotr Miedzik <qermit@sezamkowa.net>
2013-05-19 23:07:02 +0200

    test

--- src/BackgroundModel.cpp
+++ src/BackgroundModel.cpp
@@ -13,3 +13,16 @@ BackgroundModel::~BackgroundModel() {
 string BackgroundModel::name() const {
 	return _name;
 }
+
+void BackgroundModel::insert(IplImage *rawImage) {
+}
+
+void BackgroundModel::insert(Mat & m) {
+}
+
+IplImage* BackgroundModel::resultingCap() {
+	return 0;
+}
+bool BackgroundModel::useCapture() {
+	return false;
+}
